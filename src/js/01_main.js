5b5425822a451b5bef5dbb8109e4f48d06eb5636
alexsmith_proff <alexsmith_proff@mail.ru>
2022-12-28 13:39:20 +0300

    complete MainSlider

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -5,5 +5,36 @@ $(document).ready(function () {
 		$('.topnav-mobile').toggleClass('responsive');
 	});
 
+	const zero = (val, inc) => {
+		let value
+		if(inc) value = val + 1
+			else value = val
+		if (value < 10) return '0' + value
+			else value
+	}
+
+	$('.main-slider__slider').on('init reInit', (event, slick, currentSlide, nextSlide) => {
+		$('.main-slider__numbers').html(zero(0, true) + '/<span>' + zero(slick.slideCount, false) + '</span>')
+	  })
+	$('.main-slider__slider').on(`afterChange`, (event, slick, currentSlide, nextSlide) => {
+		$('.main-slider__numbers').html(zero(currentSlide, true) + '/<span>' + zero(slick.slideCount, false) + '</span>');
+	  })
+
+	$('.main-slider__slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+		fade: true,
+		speed: 5000,
+		autoplay: true
+	})
+
+	
+	
+
+	
+
 
 });
